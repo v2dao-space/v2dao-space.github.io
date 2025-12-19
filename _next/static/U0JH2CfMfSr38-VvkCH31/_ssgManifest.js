5bbda4742421adc7f7a927cf6b1b45e5748c2837
hellcase <dota2sites@gmail.com>
2025-12-19 06:18:38 +0000

    deploy

---
+++ _next/static/U0JH2CfMfSr38-VvkCH31/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcontents\u002F[slug]","\u002Fcontents\u002Fpage\u002F[page]","\u002Fsitemaps\u002Fcontents\u002Fsitemap\u002F[__metadata_id__]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
